c62b98ca065a98f62c1eac1619a0c9a55866d324
shanghaiqw <60005095+shanghaiqw@users.noreply.github.com>
2020-04-18 13:20:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,4 +7,8 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-src-git helloworld https://github.com/fw876/helloworld
+#!/bin/bash
+mkdir package/base-files/files/config
+echo 0xDEADBEEF > package/base-files/files/config/google_fu_mode
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
